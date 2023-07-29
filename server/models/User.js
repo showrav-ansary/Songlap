c5c7dd59ba6bfd289f7f6afc399b50accbb785bf
showrav-ansary <showrav.ansary.bd@gmail.com>
2023-07-29 19:12:33 +0600

    add `User` model

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstNamme: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 30,
+        },
+        lastNamme: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 30,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            max: 50,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 8,
+        },
+        picturePath: {
+            type: String,
+            default: '',
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        location: String,
+        impression: Number,
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model('User', UserSchema);
+export default User;
